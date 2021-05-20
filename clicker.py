2f8e659ef24f146d826012e1127b39221c40e860
Kinshuk-Goel <kg467@outlook.com>
2021-05-20 17:29:50 -0400

    Another commit made to the project. Inspect more for details.

--- clicker.py
+++ clicker.py
@@ -5,7 +5,8 @@ from pynput.keyboard import Listener, KeyCode
 
 delay = 0.001
 button = Button.left
-start_stop_key = KeyCode(char='a')
+start_stop_key = KeyCode(char='x')
+exit_key = KeyCode(char='x')
 
 class ClickMouse(threading.Thread) :
 	def __init__(self, delay, button):
@@ -34,3 +35,16 @@ class ClickMouse(threading.Thread) :
 mouse = Controller()
 click_thread = ClickMouse(delay, button)
 click_thread.start()
+
+def on_press(key):
+	if key == start_stop_key:
+		if click_thread.running:
+			click_thread.stop_clicking()
+		else:
+			click_thread.start_clicking()
+		elif key == exit_key:
+			click_thread.exit()
+			listener.stop()
+
+with Listener(on_press=on_press) as listener:
+	listener.join()
